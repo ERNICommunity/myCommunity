8ba2d6b44a0e858f11b946a776fe488c6f9563ba
Ruel Dognidon <rueldognidon@gmail.com>
2015-06-04 18:50:59 +0800

    New Events List Page and News Page

---
+++ myCommunity/Views/XAML/NewsListPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace myCommunity.Views.XAML
+{
+    public partial class NewsListPage : ContentPage
+    {
+        public NewsListPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
